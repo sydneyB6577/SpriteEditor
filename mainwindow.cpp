1635dd834eb6424fbecc393525b636ba2053cd27
Alex <heldalex9000@gmail.com>
2025-11-06 16:24:58 -0700

    Reference code for main canvas

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+#include <QWidget>
+#include <QImage>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QImage img(32, 32, QImage::Format_RGB32);
+
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < 32; j++) {
+            img.setPixel(i, j, (i%2==0 ? qRgb(255, 0, 0) : qRgb(0,255,0)));
+        }
+    }
+
+    QImage scaled = img.scaled(300, 300, Qt::KeepAspectRatio, Qt::FastTransformation);
+    QLabel* canvas = ui->canvasLabel;
+    QPixmap pixmap = QPixmap::fromImage(scaled);
+    canvas->setPixmap(pixmap);
+    canvas->show();
+
 }
 
 MainWindow::~MainWindow()
